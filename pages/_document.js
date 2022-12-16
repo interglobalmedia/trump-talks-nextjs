76a9d4d98502c1734ab60c030eb61fb0bb4d37a7
Maria Campbell <interglobalmedia@gmail.com>
2022-12-15 19:40:23 -0500

    Refactor doc structure for sticky footer

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Footer from '../components/layout/Footer'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <div className="site-content-wrapper">
         <Main />
+        </div>
+        <Footer />
         <NextScript />
       </body>
     </Html>
