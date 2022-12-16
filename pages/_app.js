94181d39830e24e6b32814547a7ff6c6090e112f
Maria Campbell <interglobalmedia@gmail.com>
2022-12-15 19:39:39 -0500

    Add andika font for optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import { Andika } from '@next/font/google'
+
+const andika = Andika({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={andika.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
